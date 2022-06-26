124f1e57fcba79311a47650a25affaa011d7013d
YutaSugimura <nextc3m@gmail.com>
2022-06-26 23:10:07 +0900

    first commit

---
+++ swc/src/index.ts
@@ -0,0 +1,21 @@
+export const sum = (a: number, b: number): number => a + b;
+
+export const sub = (a: number, b: number): number => a - b;
+
+export const mul = (a: number, b: number): number => a * b;
+
+export const div = (a: number, b: number): number => a / b;
+
+export const mod = (a: number, b: number): number => a % b;
+
+export const pow = (a: number, b: number): number => a ** b;
+
+export const sqrt = (a: number): number => Math.sqrt(a);
+
+export const abs = (a: number): number => Math.abs(a);
+
+export const sin = (a: number): number => Math.sin(a);
+
+export const cos = (a: number): number => Math.cos(a);
+
+export const tan = (a: number): number => Math.tan(a);
